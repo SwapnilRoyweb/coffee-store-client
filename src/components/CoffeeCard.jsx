090267fc1a03ec4355ae975208ea5cd0da2390f3
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-05-15 13:37:41 +0600

    Delete coffee crud done

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const CoffeeCard = ({ coffee }) => {
 
     const { _id, name, quantity, supplier, taste, category, details, photo } = coffee;
 
+    const handleDelete = _id => {
+        // console.log(_id);
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // console.log('deleted');
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data);
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your coffee has been deleted.',
+                                'success'
+                            )
+                        }
+                    })
+
+            }
+        })
+    }
+
     return (
-        <div className="card card-side bg-base-100 shadow-xl p-3 flex items-center justify-center">
+        <div className="card card-side bg-yellow-200 text-black shadow-xl p-3 flex items-center justify-center">
             <figure><img src={photo} alt="Movie" /></figure>
-            <div className="flex justify-evenly w-full pr-5">
-                <div>
+            <div className="flex justify-evenly w-full">
+                <div className='flex flex-col justify-center'>
                     <h2 className="card-title">Name: {name}</h2>
                     <p>{quantity}</p>
                     <p>{supplier}</p>
@@ -17,8 +51,8 @@ const CoffeeCard = ({ coffee }) => {
                 <div className="card-actions justify-end">
                     <div className="btn-group btn-group-vertical space-y-3">
                         <button className="btn btn-active">View</button>
-                        <button className="btn">Edit</button>
-                        <button className="btn bg-red-500">X</button>
+                        <Link to={`updateCoffee/${_id}`}><button className="btn">Edit</button></Link>
+                        <button className="btn bg-red-500" onClick={() => handleDelete(_id)}>X</button>
                     </div>
                 </div>
             </div>
--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,8 @@ const router = createBrowserRouter([
   },
   {
     path: 'updateCoffee',
-    element: <UpdateCoffee></UpdateCoffee>
+    element: <UpdateCoffee></UpdateCoffee>,
+    loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
   }
 ])
 
