8571819f12ff11b57946cbd6c3a71d7e2e4f2867
swapnilRoyWeb <swapnilroyweb@gmail.com>
2023-05-15 14:10:59 +0600

    some problem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+import { useState } from 'react';
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
 import CoffeeCard from './components/CoffeeCard';
 
 function App() {
 
-  const coffees = useLoaderData();
+  const loadedCoffees = useLoaderData();
+  const [coffees, setCoffees] = useState(loadedCoffees);
 
   return (
     <div className='m-20'>
       <h1 className='text-6xl text-purple-600 text-center'>Hot Cold Coffee: {coffees.length}</h1>
       <div className='grid md:grid-cols-2 gap-4 mt-5'>
         {
-          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+          coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee} coffees={coffees} setCoffees={setCoffees}></CoffeeCard>)
         }
       </div>
     </div>
